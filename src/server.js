81f7a34ca285f446171b1d6a622c8d0e0cff3dfc
codeflow <boramismn@gmail.com>
2023-02-18 10:35:21 +0900

    4. Routers

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => console.log("server listening on port 4000 🚀");
+
+app.listen(PORT, handleListening);
